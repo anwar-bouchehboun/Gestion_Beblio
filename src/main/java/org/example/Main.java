7dbe2011a912b1af23a8e30f9e3dfe24ee7cc2d3
anouar <anouar.ab95@gmail.com>
2024-08-29 17:07:41 +0100

    Creation Resp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,17 @@
 package org.example;
 
-<<<<<<< HEAD
+
 import org.example.CoucheP.ConsoleUI.ConsoleUI;
 
-=======
->>>>>>> 7c428220f77fc8fa9a46e42df29be1c4976d055b
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-<<<<<<< HEAD
+
         ConsoleUI Menu = new ConsoleUI();
         Menu.Start();
-=======
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
->>>>>>> 7c428220f77fc8fa9a46e42df29be1c4976d055b
     }
 }
